b08c11bf51ecee308d8e955ea2278ff0a4130e6d
Jérémy <jeremy.bargorn@epfl.ch>
2022-06-03 11:01:54 +0200

    tada

--- Assets/Scripts/PongScripts/Behaviors/MoveWithKeyboard.cs
+++ Assets/Scripts/PongScripts/Behaviors/MoveWithKeyboard.cs
@@ -8,6 +8,9 @@ public class MoveWithKeyboard : AgentBehaviour
     float horizontal;
     float vertical;
     public Powers powers;
+    Steering steering = new Steering();
+    bool collisionBehavior;
+
 
     // Start is called before the first frame update
     void Start()
@@ -39,12 +42,16 @@ public class MoveWithKeyboard : AgentBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnCollisionEnter(Collision collision) {
         agent.ActivateDirectionalHapticFeedback();
+        foreach (ContactPoint contact in collision.contacts)
+        steering.linear = collision.contacts[0].normal.normalized*agent.maxAccel;
+        collisionBehavior = true;     
     }
 
-    private void OnCollisionExit(Collision other) {
+    void OnCollisionExit(Collision collision){
         agent.DeActivateDirectionalHapticFeedback();
+        collisionBehavior = false; 
     }
 
     public override Steering GetSteering()
@@ -53,19 +60,18 @@ public class MoveWithKeyboard : AgentBehaviour
         //ne peut bouger que si la partie a commencer
         if (gameManager.game_as_start())
         {
-            if (gameManager.getPlayerCommand(gameObject.tag) == "wasd")
-            {
-                horizontal = Input.GetAxis("Horizontal2");
-                vertical = Input.GetAxis("Vertical2");
-            }
-            else
-            {
-                horizontal = Input.GetAxis("Horizontal");
-                vertical = Input.GetAxis("Vertical");
+            if(!collisionBehavior){
+                if (gameManager.getPlayerCommand(gameObject.tag) == "wasd"){
+                    horizontal = Input.GetAxis("Horizontal2");
+                    vertical = Input.GetAxis("Vertical2");
+                }
+                else{
+                    horizontal = Input.GetAxis("Horizontal");
+                    vertical = Input.GetAxis("Vertical");
+                }
             }
         }
 
-        Steering steering = new Steering();
 
         steering.linear = new Vector3(horizontal, 0, vertical) * agent.maxAccel;
         steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear, agent.maxAccel));
